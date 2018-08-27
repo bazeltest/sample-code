6a3a32331f29325848f78d3bb094cdfd8d57079c
bazeltest <abqadeer077@gmail.com>
2018-08-27 13:30:32 +0500

    Changes in tests

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -2,4 +2,7 @@ describe('All tests Should Pass', () => {
     it('Should pass', () => {
         expect(1).toEqual(1)
     })
+    it('Should fail', () => {
+        expect(1).toEqual(2)
+    })
 })
