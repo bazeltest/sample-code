780115a684a17ddb33ac8f42370af88e66a2f686
bazeltest <abqadeer077@gmail.com>
2018-08-27 13:23:29 +0500

    Changes in jest config

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     testRegex: "./tests/[a-zA-Z0-9]{1,40}.spec.ts",
 
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testURL: "http://localhost/",
     testPathIgnorePatterns: [
         "<rootDir>/node_modules/"
     ],
